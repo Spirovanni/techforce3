42bf43ae888278f6ae35b92e5a5e2c4a3fc217a5
Xavier Martinez <blackshieldsx@gmail.com>
2019-11-29 14:06:30 -0800

    Created Organization in Models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Organisation(models.Model):
+    orgName = models.CharField(max_length=32)
+    orgDescription = models.TextField(max_length=360)
+
+
+class Rating(models.Model):
+    organisation = models.ForeignKey(Organisation)
