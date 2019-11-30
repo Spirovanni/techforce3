6b94feb8fefd9b4c189faca001b536d67e3b4182
Xavier Martinez <blackshieldsx@gmail.com>
2019-11-29 18:44:08 -0800

    Set up views for serializers

--- api/views.py
+++ api/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Organisation, Rating
+from .serializers import OrganisationSerializer, RatingSerializer
 
-# Create your views here.
+
+class OrganisationViewSet(viewsets.ModelViewSet):
+    queryset = Organisation.objects.all()
+    serializer_class = (OrganisationSerializer, )
+
+
+class RatingViewSet(viewsets.ModelViewSet):
+    queryset = Rating.objects.all()
+    serializer_class = (RatingSerializer, )
