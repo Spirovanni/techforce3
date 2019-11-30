1c75f80aa58496425b0b70ebc73b358f2e64d9f0
Xavier Martinez <blackshieldsx@gmail.com>
2019-11-30 13:00:33 -0800

    Sync up the serializer

--- api/serializers.py
+++ api/serializers.py
@@ -2,10 +2,16 @@ from rest_framework import serializers
 from . import models
 from django.contrib.auth.models import User
 
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.User
         fields = ['id', 'username', 'password']
+        extra_kwargs = {'password': {'write_only': True, 'required': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
 
 
 class OrganisationSerializer(serializers.ModelSerializer):
