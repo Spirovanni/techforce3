7efc9503cf88f3fcb922ff2059ad890b07323c63
Xavier Martinez <blackshieldsx@gmail.com>
2019-11-30 12:07:12 -0800

    Added router.register('users', UserViewSet) in urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from rest_framework import routers
 from django.conf.urls import include
-from .views import OrganisationViewSet, RatingViewSet
+from .views import OrganisationViewSet, RatingViewSet, UserViewSet
 
 router = routers.DefaultRouter()
+router.register('users', UserViewSet)
 router.register('organisations', OrganisationViewSet)
 router.register('ratings', RatingViewSet)
 
