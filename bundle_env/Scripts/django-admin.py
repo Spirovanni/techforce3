55b93147da27a8ca04c7e23209ea01c8f74666d7
Xavier Martinez <blackshieldsx@gmail.com>
2019-12-01 11:32:09 -0800

    Added from bundle

---
+++ bundle_env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\cyran\dev\techforce2\techforce3\bundle_env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
